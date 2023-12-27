2dd78e2247cad9f8f146e9bd20e437d45846cc67
iftekharr001 <iftekharr001@gmail.com>
2023-12-27 01:13:13 -0800

    created website layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from "./components"
 
-function App() {
+const App = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <h3 className="text-red-500">Hello</h3>
-    </>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
