267942fcfffd0f8731a2ed19d43910353c2bff0d
iftekharr001 <iftekharr001@gmail.com>
2023-12-27 11:57:41 -0800

    created navbar and hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,78 @@
-
-import {styles} from "../styles";
-// import {navLink} from "../contents";
-import {logo, menu, close} from "../assets";
+import { styles } from "../styles";
+import { navLinks } from "../contents";
+import { logo, menu, close } from "../assets";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const Navbar = () => {
-  const [active, setActive] = useState("")
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto"> 
-        <Link 
-        to="/" 
-        className="flex items-center gap-2" 
-        onClick={() => {
-          setActive("");
-          window.scrollTo(0, 0)
-        }}>
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-12 h-12 object-contain" />
+          <p className="text-white text-[25px] font-bold cursor-pointer">
+            Master In MERN
+          </p>
         </Link>
+        <div>
+          <ul className="list-none hidden sm:flex flex-row gap-10">
+            {navLinks.map((link) => (
+              <li
+                key={link.id}
+                className={`${
+                  active === link.title ? "text-[#3b286f]" : "text-white"
+                }  hover:text-[#271a49] cursor-pointer text-[15px] hover:text-[18px] font-medium`}
+                onClick={() => setActive(link.title)}
+              >
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            ))}
+          </ul>
+          <div className="sm:hidden flex flex-1 justify-end items-center">
+            <img
+              className="w-[28px] h-[28px] cursor-pointer object-contain"
+              onClick={() => setToggle(!toggle)}
+              src={toggle ? close : menu}
+              alt="menu"
+            />
+            <div
+              className={`${
+                !toggle ? "hidden" : "flex"
+              } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl `}
+            >
+              <ul className="list-none flex justify-end items-start flex-col gap-4">
+                {navLinks.map((link) => (
+                  <li
+                    key={link.id}
+                    className={`${
+                      active === link.title ? "text-[#e2c234]" : "text-white"
+                    }  hover:text-[#a2860d] font-poppins cursor-pointer text-[15px] hover:text-[18px] font-medium`}
+                    onClick={() => {
+                      setToggle(!toggle);
+                      setActive(link.title);
+                    }}
+                  >
+                    <a href={`#${link.id}`}>{link.title}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
